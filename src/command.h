54e0261cb25c1a7df353b95452d4e2df88008ea5
nsing013 <nsing013@ucr.edu>
2017-02-17 20:26:14 -0800

    Update command.h

--- src/command.h
+++ src/command.h
@@ -1,19 +1,28 @@
+#ifndef COMMAND_H
+#define COMMAND_H
 #include <iostream>
+#include <vector>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 using namespace std;
 /*this is the Command BASE CLASS and it has 2 SUBCLASSES
 "BuiltIn" and "Executable" The BuiltIn has another subclass 
 called "Exit" which is handles the Built In Command Exit */
 
 class command{
-    
+  public:
+    command(vector<string> cmd);
+    void parse();
+    void execCmd();
+    bool didCommand();
+  private:
+    vector<string> input;
+    bool failedExecute;
+    char** arguments;
 };
 
-class BuiltIn : public command{
-  public:
-    BuiltIn();
-};
-
-class Executable : public command{
-  public:
-    Executable();
-};
+#endif
