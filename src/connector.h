42c3861a936f971c2bb28fef00b236f974e7c99f
nsing013 <nsing013@ucr.edu>
2017-02-17 20:29:50 -0800

    Update connector.h

--- src/connector.h
+++ src/connector.h
@@ -1,4 +1,7 @@
+#ifndef CONNECTOR_H
+#define CONNECTOR_H
 #include <iostream>
+#include <string>
 #include "command.h"
 using namespace std;
 
@@ -11,6 +14,8 @@ class connector
     connector();
     void setCommand(command* cmd);
     void execute();
+    virtual string getType() = 0;
+    bool getBool();
   protected:
     command* child;
 };
@@ -19,22 +24,40 @@ class head : public connector
 {
   public:
     head();
+    string getType();
+    // bool getBool();
+  private:
+    string type;
 };
 
 class opOr : public connector
 {
   public:
     opOr();
+    string getType();
+    // bool getBool();
+  private:
+    string type;
 };
 
 class opAnd : public connector
 {
   public:
     opAnd();
+    string getType();
+    // bool getBool();
+  private:
+    string type;
 };
 
 class opSemi : public connector
 {
   public:
     opSemi();
+    string getType();
+    // bool getBool();
+  private:
+    string type;
 };
+
+#endif
