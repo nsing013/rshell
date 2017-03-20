4d83b18c951b67ea0b822a2590c103b128926b38
nsing013 <nsing013@ucr.edu>
2017-03-19 23:48:31 -0700

    Update multi_command.sh

--- tests/multi_command.sh
+++ tests/multi_command.sh
@@ -3,4 +3,6 @@ ls -a ; ls -l
 touch file ; echo world
 [ -e main.cpp ] && echo hello world
 test -e main.cpp || ls 
+cat < main.cpp | tr a-z A-Z > cat.out
+cat < main.cpp | tr A-Z a-z | tee cat.out | tr a-z A-Z > cat2.out
 exit
