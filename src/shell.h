40206f97c19ef5a9ad480aa86cd7cb6b3076749e
nsing013 <nsing013@ucr.edu>
2017-03-19 23:18:09 -0700

    Update shell.h

--- src/shell.h
+++ src/shell.h
@@ -3,12 +3,10 @@
 #include <iostream>
 using namespace std;
 
-/*This class is the shell class. IT HAS NO CHILDREN!!! 
-BUT, it outputs a prompt, takes a command, and outputs a 
-prompt again. We use a linked list to store the connectors 
-entered by the user */
+/*This class is the shell class. IT HAS NO CHILDREN!!! BUT, it outputs a prompt,
+takes a command, and outputs a prompt again. We use a linked list to store the 
+connectors entered by the user */
 
-//classes go here
 class shell{
   public:
     shell();
